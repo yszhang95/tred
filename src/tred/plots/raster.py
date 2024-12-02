dadfbfdc6e15805d84585d2285c9e1ff302efcc6
Brett Viren <brett.viren@gmail.com>
2024-12-02 14:21:35 -0500

    3D depo plots including k3d driver

--- src/tred/plots/raster.py
+++ src/tred/plots/raster.py
@@ -7,15 +7,106 @@ from tred.util import make_points
 from tred.drift import drift
 from tred.raster import depos
 
+def make_3d():
+    vdim = 3                    # 3 spatial (vector) dimensions
+
+    locs = torch.tensor([
+        [-10.0, -5.0, 0.0, 5.0, 10.0, 20], # x
+        [-20.0, -10.0, 0.0, 10.0, 20.0, 40], # y
+        [-20.0, -10.0, 0.0, 10.0, 20.0, 40], # z
+    ]).T
+
+    velocity = -1               # from high location value to low
+    target = 0
+    diffusion = torch.tensor([1,2,2])
+    lifetime = 10
+    nsigma = torch.tensor([3.0]*vdim)
+    
+    centers, times, sigmas, charges = drift(locs, velocity, diffusion, lifetime)
+
+    centers[:,0] = times        # from (x,y),t to (t,y),x=x_resp
+    sigmas[:0] /= abs(velocity) # sigma_x to sigma_t
+
+    tgrid = 1
+    ygrid = 2
+    zgrid = 2
+
+    grid = torch.tensor([tgrid, ygrid, zgrid])
+
+    rasters, offsets = depos.binned(grid, centers, sigmas, charges, nsigma=nsigma)
+    return grid, rasters, offsets
+
+
+def sum_3d(grid, rasters, offsets):
+    '''
+    Do a serial sum of offset rasters and return array spanning grid.
+    '''
+    rshape = torch.tensor(rasters.shape[1:])
+    ends = offsets + rshape + 1 # add one to show we aren't truncating raster
+
+    mincorner,_ = torch.min(offsets, dim=0)
+    maxcorner,_ = torch.max(ends, dim=0)
+
+    full_shape = maxcorner - mincorner
+    full_raster = torch.zeros(tuple(full_shape.tolist()))
+
+    for r, o in zip(rasters, offsets):
+        b = o - mincorner
+        e = b + rshape
+        full_raster[b[0]:e[0], b[1]:e[1], b[2]:e[2]] += r
+    return full_raster
+
+
+def plot_depo_k3d():
+    '''
+    Generate some 3D depos, drift, raster and plot with k3d.
+
+    Note, this MUST be run from jupyter notebook.
+
+    $ uv run --with jupyter jupyter notebook
+
+    Then in a notebook cell:
+    
+    import tred.plots.raster
+    tred.plots.raster.plot_depo_k3d(None)
+    '''
+    import k3d
+    grid, rasters, offsets = make_3d()
+    full_raster = sum_3d(grid, rasters, offsets)
+
+    vol = k3d.volume(full_raster)
+    plot = k3d.plot()
+    plot += vol
+    plot.display()
+
+
+def plot_depo_3d(out):
+    '''
+    Generate some 3D depos, drift, raster and plot.
+    '''
+    grid, rasters, offsets = make_3d()
+
+    full_raster = sum_3d(grid, rasters, offsets)
+
+    fig, axes = make_figure('3D depo rasters', nrows=3)
+
+    for axis in range(3):
+        ax = axes[axis]
+        two = torch.sum(full_raster, axis=axis)
+        im = ax.imshow(two, aspect='auto',
+                       norm=LogNorm(vmin=0.01, vmax=torch.max(full_raster)))
+        ax.set_title(f'sum over axis {axis}')
+
+        fig.colorbar(im)
+    fig.tight_layout()
+
+    out.savefig()
+
 def plot_depo_2d(out):
     '''
     Generate some 2D depos, drift, raster and plot.
     '''
-    npoints = 3
     vdim = 2                    # 2 spatial (vector) dimensions
-
-    xmin, xmax = -1, 10
-    ymin, ymax = -10, 10
 
     locs = torch.tensor([
         [-10.0, -5.0, 0.0, 5.0, 10.0, 20], # x
@@ -86,10 +177,8 @@ def plot_depo_1d(out):
     '''
     Generate some 1D depos, drift, raster and plot.
     '''
-    npoints = 3
     vdim = 1
 
-    xmin, xmax = -1, 10
     locs = torch.tensor([-1.0, 0.0, 1.0, 4.0, 8.0])
 
     velocity = -1               # from high location value to low
@@ -145,7 +234,7 @@ def plot_depo_1d(out):
 
 def plots(out):
     glb = globals()
-    for d in [1, 2]:
+    for d in [1,2,3]:
         meth = glb[f'plot_depo_{d}d']
         meth(out)
 
