5b95a206dd22416c3818ae2562d9eac5c2426912
Yousen Zhang <yzhang11@bnl.gov>
2025-03-21 23:35:22 -0400

    Added plots for crop_batched, union_bounds; seen bug in crop_batched

--- tests/sparse/test_indexing.py
+++ tests/sparse/test_indexing.py
@@ -2,10 +2,10 @@
 '''
 Supplemental functions to tests/test_indexing.py
 '''
-
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
-
-from tred.indexing import crop
+from tred.indexing import shape_meshgrid, crop, crop_batched, union_bounds
 
 def test_crop():
     offset = torch.tensor((1,2))
@@ -22,3 +22,48 @@ def test_crop():
     inds = crop(offset, inner, outer)
 
     parent2.flatten()[inds] = child.flatten()
+
+def box2d(c0, c1):
+    return np.array([[c0[0], c0[0], c1[0], c1[0], c0[0]],
+                     [c0[1], c1[1], c1[1], c0[1], c0[1]]])
+
+def plot_crop_batched_2d():
+    inner = torch.tensor(((2,2), (3,3)))
+    outer = torch.tensor((10, 10))
+    offsets = torch.tensor([(3,3), (1,2)])
+
+    grid = shape_meshgrid(outer)
+
+    data = torch.zeros([len(offsets),] + outer.tolist())
+
+    inds = crop_batched(offsets, inner, outer)
+    data.flatten()[inds] = 1
+
+    fig, axes = plt.subplots(nrows=len(offsets), ncols=1, figsize=(8, 6*len(offsets)))
+    for i in range(len(offsets)):
+        axes[i].scatter(grid[0], grid[1], c=data[i], label=f'ibatch {i}')
+        axes[i].legend()
+    plt.savefig('crop_batched_2d.png')
+
+def plot_union_bounds_2d():
+    shape = (2,2)
+    offsets = torch.tensor([(3,3), (1,2)])
+
+    pmin, pmax = union_bounds(shape, offsets)
+    pmax = pmin + pmax
+    boundx, boundy = box2d(pmin.numpy(), pmax.numpy())
+    plt.plot(boundx, boundy, 'o-', label='bound')
+    for i in range(len(offsets)):
+        x, y = box2d(offsets[i].numpy(), (offsets[i] + torch.tensor(shape)).numpy())
+        plt.plot(x, y, 'o-', label=f'ibatch{i}')
+    plt.legend()
+    plt.savefig('union_bounds_2d.png')
+
+
+def main():
+    test_crop()
+    plot_union_bounds_2d()
+    plot_crop_batched_2d()
+
+if __name__ == '__main__':
+    main()
