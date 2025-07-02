b93e4e9641cb04bf49b9977f9b06932b6a2b5e25
Yousen Zhang <yzhang11@bnl.gov>
2025-07-02 13:53:45 -0400

    larnd-sim comparison

---
+++ tests/playground/for_larndsim/run_2tks.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+uv run tred -c config_for_larndsim_newgeo_fixthres_nonoise.yaml fullsim -i /home/yousen/Public/ndlar_shared/data/larndsim_neighboring_pixels_20250623/mu_p_2tks.hdf5 -o for_larndsim_2tks.npz
