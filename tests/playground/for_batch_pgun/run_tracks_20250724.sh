80694654c1540ed5c240a35d66def46f8e446805
Yousen Zhang <yzhang11@bnl.gov>
2025-07-28 17:47:56 -0700

    run files

---
+++ tests/playground/for_batch_pgun/run_tracks_20250724.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# skip # in the file
+event_ids=( $(grep -v '^#' /home/yousen/Public/ndlar_shared/data_reflowv5_20250722/event_list.txt) ) # works
+for i in ${event_ids[@]}; do
+    # skip # in the file
+    if [[ $i == \#* ]]; then
+        echo "Skipped comment line: $i"
+        continue
+    fi
+  echo "Processing event ID: $i"
+  InFile="/home/yousen/Documents/NDLAr2x2/MuonLArSim/build/pgun_mu_3GeV_2mm_event_id${i}.hdf5"
+  OutFile="/home/yousen/Documents/NDLAr2x2/MuonLArSim/build/pgun_mu_3GeV_2mm_event_id${i}_delay18_tred.npz"
+  echo "uv run tred -c config_for_pgun.yaml fullsim -i $InFile -o $OutFile"
+  CUDA_LAUNCH_BLOCKING=1 uv run tred -c config_for_pgun_delay18.yaml fullsim -i $InFile -o $OutFile
+done
